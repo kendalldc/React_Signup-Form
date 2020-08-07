b42115f54b15700d4ca9eab1d9d357b05ed3eb47
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-06 20:53:34 -0400

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -67,18 +67,23 @@ this.setState({submitted:true})
             <div className="form">
                 <form onSubmit={this.handleSubmit}>
 <div className='username'>
+    <label>Username:</label>
  <input name='username' placeholder='Username' type="text" value={this.state.username} onChange={this.handleChange} required/> <br/>
  </div>
- 
+ <br/>
   <div className='password'>
+  <label>Password:</label>
  <input name='password' placeholder='Password' type="password" value={this.state.password} onChange={this.handleChangePassword} required/> <br/>
  </div>
-
+ <br/>
  <div className='age'>
+ <label>Age:</label>
  <input name='age' placeholder='Age' type="number" value={this.state.age} onChange={this.handleChangeAge} required/> <br/>
  </div>
- 
+ <br/>
  <div className='gender'>
+ <label>Gender:</label>
+
  <select  name='gender' placeholder='Gender'  value={this.state.gender} onChange={this.handleSelect} required>
  <option disabled hidden>Gender</option>
 <option>Female</option>
@@ -86,9 +91,13 @@ this.setState({submitted:true})
 <option>Other</option>
 </select> <br/>
 </div>
+<br/>
+
 <div className='occupation'>
+<label>Occupation:</label>
      <input name='Occupation' placeholder='Occupation' type="text" value={this.state.name} onChange={this.handleChangeOccupation}required/><br/>
  </div>
+ <br/>
 
 <div className='createAccount'>
 <button type="submit">Create Account</button>
