c239729cbff892f8fbfff94d99b0fbc15a229126
kendalldc <67480485+kendalldc@users.noreply.github.com>
2020-08-09 13:22:01 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from './Components/Form';
+import './App.css';
+
+function App(){
+  
+  return(
+<div className='App'>
+  <h1>Register</h1>
+ 
+  <Form />
+</div>
+      );
+  
+}
+
+export default App;
